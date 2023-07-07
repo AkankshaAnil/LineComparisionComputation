2fadc425d9fed711001fb1fc04bf5c8c40dd2567
AkankshaAnil <akankshakodgirwar07@gmail.com>
2023-07-07 22:22:31 +0530

    [AkankshaAnil] calculating length of line

--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿using System;
+
+namespace LineComparisionComputation
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello User - Welcome to Line Comparision Computation");
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User - Welcome to Line Comparision Computation");
+
+
+            Console.WriteLine("Please enter X-Coordinate of First EndPoint: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter Y-Coordinate of First EndPoint: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter X-Coordinate of Second EndPoint: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter Y-Coordinate of Second EndPoint: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double coordinatex = x2 - x1;
+            double coordinatey = y2 - y1;
+
+            double length = Math.Sqrt((coordinatex * coordinatex) + (coordinatey * coordinatey));
+            Console.WriteLine("The Length Of Line Is: " + length);
+        }
     }
 }
